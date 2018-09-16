1971996bd0f79e9fd0559cfa7a15dfaa46007a65
Pamir_Ghimire <pamirghimire@gmail.com>
2018-09-16 17:59:33 +0200

    found desired bezier curve configurations

--- src/rendering/blenderScripts/blenderExecfile.py
+++ src/rendering/blenderScripts/blenderExecfile.py
@@ -1,9 +1,2 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Sep  9 18:51:40 2018
-
-@author: ghimire
-"""
-
-filename = "/home/bokoo/Desktop/sft_dl_3dc/src/rendering/blenderScripts/prospecting.py"
+filename = '/home/ghimire/Desktop/sft_dl_3dc/src/rendering/blenderScripts/prospecting.py'
 exec(open(filename).read())
--- src/rendering/blenderScripts/prospecting.py
+++ src/rendering/blenderScripts/prospecting.py
@@ -11,7 +11,7 @@ import numpy as np
 #-------------
 objFilename = 'horses'
 objFileType = '.obj'
-objFileDir = '/home/bokoo/Desktop/sft_dl_3dc/data/3dObjs/'
+objFileDir = '/home/ghimire/Desktop/sft_dl_3dc/data/3dObjs/'
 objFilePath = objFileDir + objFilename + objFileType
 
 importedObj = bpy.ops.import_scene.obj(filepath=objFilePath)
@@ -67,18 +67,18 @@ bpy.context.scene.update()
 #my3dobj.matrix_world = mathutils.Matrix.Rotation(np.pi/10.0, 4, Vector((0.0, 1.0, 0.0)))*my3dobj.matrix_world
 #bpy.context.scene.update()
 
-# axis-pivot-angle
-pivot_world = mycam.matrix_world * Vector((0.0, 0.0, -20.0))
-axis_world = Vector((0.0, 0.0, 1.0))
-angle = 10.0*(2*np.pi/180.0)
-# pivot-axis-angle = subtract pivot, then axis-angle rotation
-my3dobj.location -= pivot_world
-bpy.context.scene.update()
-my3dobj.matrix_world = mathutils.Matrix.Rotation(angle, 4, axis_world) * my3dobj.matrix_world
-my3dobj.location += pivot_world
-bpy.context.scene.update()
-
-
+## axis-pivot-angle
+#pivot_world = mycam.matrix_world * Vector((0.0, 0.0, -20.0))
+#axis_world = Vector((0.0, 0.0, 1.0))
+#angle = 10.0*(2*np.pi/180.0)
+## pivot-axis-angle = subtract pivot, then axis-angle rotation
+#my3dobj.location -= pivot_world
+#bpy.context.scene.update()
+#my3dobj.matrix_world = mathutils.Matrix.Rotation(angle, 4, axis_world) * my3dobj.matrix_world
+#my3dobj.location += pivot_world
+#bpy.context.scene.update()
+#
+#
 # euler angles
 
 #-------------
@@ -109,11 +109,13 @@ bpy.context.scene.update()
 my3dobj.location = Vector((0.0, 0.0, 0.0))
 my3dobj.rotation_euler = Vector((np.pi/2.0, 0.0, 0.0)) 
 
-
-
 coordinates = [
-    ((-1, 0, 0), (-0.7, 0, 0), (-1, 0.5521, 0)),
-    ((0, 1, 0), (-0.5521, 1, 0), (0, 0.7, 0))]
+    ((-5, 0, 0), (-10, -5, 0), (0, 5, 0)),
+    ((5, 0, 0), (0, 0, 0), (10, 0, 0))]
+
+#coordinates = [ #center, short arm, long arm
+#    ((-1, 0, 0), (-0.7, 0, 0), (-1, 0.5521, 0)),
+#    ((0, 1, 0), (-0.5521, 1, 0), (0, 0.7, 0))]
 #    ,
 #    ((0, 0, 0), (0, 0.3, 0), (-0.3, 0, 0))
 #]
@@ -136,50 +138,50 @@ def MakeCurveQuarter(objname, curvename, cList, origin=(0,0,0)):
         point.co = knot
         point.handle_left = h1
         point.handle_right = h2
-        point.handle_left_type = 'FREE'
-        point.handle_right_type = 'FREE'
+        point.handle_left_type = 'ALIGNED'
+        point.handle_right_type = 'ALIGNED'
 
     polyline.use_cyclic_u = False 
 
-MakeCurveQuarter("NameOfMyCurveObject", "NameOfMyCurve", coordinates)  
+MakeCurveQuarter("myBCurveObject", "myBCurve", coordinates)  
 
-# add a bezier curve to the context : shift+a > curve > Bezier
-# Curve Data:
-curveData = bpy.data.curves.new('myBCurve', type='CURVE')
-curveData.dimensions = '3D'
-curveData.resolution_u = 2
-# set its 4 coordinates ([top-left, top-right, bottom-left, bottom-right]) 
-# when viewdir is s.t. 'y' increases along it (units: centimeters)
-coords = [(8.0601, -0.742, 0)]#, (1.858, -19.89, 0.167)]#, (-164.67,-28.59,-0.265), (-37.064, 27.821, 0.2583)] 
-# make sure that nothing is selected in the 'path/curve deform' in curve shape
-curveData.use_radius = False 
-
-# map coords to spline
-polyline = curveData.splines.new('BEZIER')
-polyline.bezier_points.add(len(coords))
-for i, coord in enumerate(coords):
-    x,y,z = coord
-    polyline.bezier_points[i].co = (x, y, z)
-
-# create Object
-myBCurveObj = bpy.data.objects.new('myBCurve', curveData)
-
-# attach to scene and validate context
-scn = bpy.context.scene
-scn.objects.link(myBCurveObj)
-scn.objects.active = myBCurveObj
-myBCurveObj.select = True
+## add a bezier curve to the context : shift+a > curve > Bezier
+## Curve Data:
+#curveData = bpy.data.curves.new('myBCurve', type='CURVE')
+#curveData.dimensions = '3D'
+#curveData.resolution_u = 2
+## set its 4 coordinates ([top-left, top-right, bottom-left, bottom-right]) 
+## when viewdir is s.t. 'y' increases along it (units: centimeters)
+#coords = [(8.0601, -0.742, 0)]#, (1.858, -19.89, 0.167)]#, (-164.67,-28.59,-0.265), (-37.064, 27.821, 0.2583)] 
+## make sure that nothing is selected in the 'path/curve deform' in curve shape
+#curveData.use_radius = False 
+#
+## map coords to spline
+#polyline = curveData.splines.new('BEZIER')
+#polyline.bezier_points.add(len(coords))
+#for i, coord in enumerate(coords):
+#    x,y,z = coord
+#    polyline.bezier_points[i].co = (x, y, z)
+#
+## create Object
+#myBCurveObj = bpy.data.objects.new('myBCurve', curveData)
+#
+## attach to scene and validate context
+#scn = bpy.context.scene
+#scn.objects.link(myBCurveObj)
+#scn.objects.active = myBCurveObj
+#myBCurveObj.select = True
 
 # change 'dimension' (in n-panel) of the added bezier curve
 #   x = 10 cm, y = 3.7141 cm, z = 0 cm
 #myBCurveObj.dimensions = Vector((10.0, 3.7141, 0))
-current_x, current_y, current_z =  myBCurveObj.dimensions
-myBCurveObj.dimensions = [10.0, 3.7141, current_z]
-
-
-# change the pose of the curve (0, 0, 0, 0, 90, -90)
-myBCurveObj.location = Vector((0.0, 0.0, 0.0))
-myBCurveObj.rotation_euler = Vector((0, np.pi/2, -np.pi/2))
+#current_x, current_y, current_z =  myBCurveObj.dimensions
+#myBCurveObj.dimensions = [10.0, 3.7141, current_z]
+#
+#
+## change the pose of the curve (0, 0, 0, 0, 90, -90)
+#myBCurveObj.location = Vector((0.0, 0.0, 0.0))
+#myBCurveObj.rotation_euler = Vector((0, np.pi/2, -np.pi/2))
 
 
 # change the 'shape' of the curve from 3D to 2D
@@ -202,7 +204,7 @@ bpy.ops.object.modifier_apply(apply_as='DATA', modifier="mysubsurf")
 #-------------
 # 7 save renders
 #-------------
-renderFilePath = '/home/bokoo/Desktop/sft_dl_3dc/data/training_defRenders/testRender.jpg'
+renderFilePath = '/home/ghimire/Desktop/sft_dl_3dc/data/training_defRenders/testRender.jpg'
 bpy.data.scenes["Scene"].render.filepath = renderFilePath
 bpy.ops.render.render( write_still=True )
 
