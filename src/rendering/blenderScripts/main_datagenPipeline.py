c89b4edea0e91abf4493b7c8f1aa078296c920f2
Pamir_Ghimire <pamirghimire@gmail.com>
2018-09-16 19:35:49 +0200

    initial datagen pipeline script

---
+++ src/rendering/blenderScripts/main_datagenPipeline.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Produces a set of renders of a template by deforming it using Blender's 
+Bezier Curve modifier, and assigning it random poses in the camera space 
+
+-Pamir Ghimire, Spetember 16, 2018
+"""
+# blender imports
+# setup lights in the environment
+# setup camera intrinsics
+
+# import 3d object
+# place it at a pre-determined pose
+# add a bezier curve modifier to the scene with pre-chosen configurations
+# add a curve modifier to the object
+# set the bezier curve as curve modifier's object
+# move the object along the modifier curve, so that it's centered
+
+# for n. of desired renders 
+    # change rotation of the curve modifier
+    # move the camera to a random pose wrt the object
+    # save the render
